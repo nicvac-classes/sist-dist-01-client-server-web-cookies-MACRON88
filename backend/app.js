14d80044a7d5c88d7e18fd3587147818645f7fc0
Marcon_Davide88 <160834806+MACRON88@users.noreply.github.com>
2024-11-07 17:53:59 +0000

    consegna

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
-// Configurazione EJS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name;
+    if (name) {
+        res.render('greet', { message: 'Bentornato', name: name });
+    } else {
+        res.render('form');
+    }
 });
 
-app.post('/greet', (req, res) => {
+
+app.post('/', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    res.cookie('name', name);
+    res.render('greet', { message: 'Benvenuto', name: name });
+});
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('name');
+    res.redirect('/');
 });
 
 const PORT = 3000;
